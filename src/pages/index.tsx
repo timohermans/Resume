20a330018a922cd7a964c16b6428aebe07ee8a41
Timo Hermans <tmhermans@gmail.com>
2023-11-08 17:15:10 +0100

    Style the home page a little

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,29 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 import styles from '../styles/Home.module.css';
 import TimoImage from '../assets/images/timo-pres.jpg';
+import EnglishImage from '../assets/images/english.png';
+import DutchImage from '../assets/images/dutch.png';
 
 const Home = () => {
     const { t } = useTranslation();
     return (
         <div className={styles.container}>
-
             <header className={styles.header}>
-                <img
-                    src={TimoImage}
-                    className={styles.headerImage}
-                />
+                <img src={TimoImage} className={styles.headerImage} />
             </header>
 
             <main className={styles.main}>
                 <h1 className={styles.title}>{t('home.welcome')}</h1>
 
-                <p className={styles.description}>{t('home.explanation')}</p>
-
                 <div className={styles.grid}>
                     <a href="/resume/nl" className={styles.card}>
-                        <h3>Nederlands</h3>
-                        <p>CV in het Nederlands</p>
+                        <img className={styles.languageLogo} src={DutchImage} alt="dutch" />
+                        Nederlands
                     </a>
 
                     <a href="/resume/en" className={styles.card}>
-                        <h3>English</h3>
-                        <p>My resume in English</p>
+                        <img className={styles.languageLogo} src={EnglishImage} alt="english" />
+                        English
                     </a>
                 </div>
             </main>
